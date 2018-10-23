260b506c3af4d204361f426e89020fd77491f541
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-10-23 15:49:57 +0900

    update

--- OpenHack181023/ChomadoFunc.cs
+++ OpenHack181023/ChomadoFunc.cs
@@ -15,15 +15,15 @@ namespace OpenHack181023
     public static class ChomadoFunc
     {
         [FunctionName("ChomadoFunc")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string productId = req.Query["productId"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            productId = productId ?? data?.productId;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //productId = productId ?? data?.productId;
 
             return productId != null
                 ? (ActionResult)new OkObjectResult($"The product name for your product id {productId} is Starfruit Explosion")
